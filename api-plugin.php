db21f2f77a5f479640e2359c0f8bf857b88546e1
Ilya Shirokov <asd.mat@mail.ru>
2023-11-24 12:51:34 +0300

    update

--- api-plugin.php
+++ api-plugin.php
@@ -6,8 +6,10 @@ class NameApi
 {
     public $api_url;
 
+    // Изменен метод listVacancies для улучшения читаемости и структуры
     public function listVacancies($post, $vid = 0)
     {
+        // Добавлена проверка типа входного параметра и исключение при ошибке
         if (!is_object($post)) {
             throw new \InvalidArgumentException("Parameter 'post' must be an object.");
         }
@@ -16,7 +18,9 @@ class NameApi
         $found = null;
         $ret = array();
 
+        // Используем цикл do-while вместо goto для улучшения читаемости
         do {
+            // Параметры запроса сформированы с помощью http_build_query для безопасности и читаемости
             $params = http_build_query([
                 'status' => 'all',
                 'id_user' => $this->selfGetOption('superjob_user_id'),
@@ -27,35 +31,52 @@ class NameApi
                 'count' => 100
             ]);
 
+            // Вызов apiSend для получения данных
             $res = $this->apiSend($this->api_url . '/hr/vacancies/?' . $params);
             $res_o = json_decode($res);
 
+            // Обработка ошибок запроса к API
             if ($res === false || !is_object($res_o) || !isset($res_o->objects)) {
                 throw new \RuntimeException("API request failed or returned invalid data.");
             }
 
+            // Перебор полученных объектов
             foreach ($res_o->objects as $value) {
+                // Проверка на наличие конкретного ID вакансии
                 if ($vid > 0 && $value->id == $vid) {
                     $found = $value;
-                    break 2;
+                    break 2; // Выход из цикла и do-while сразу
                 }
 
+                // Сохранение всех объектов, если конкретный ID не указан
                 $ret[] = $value;
             }
 
             $page++;
         } while (!$found && $res_o->more);
 
+        // Возврат найденного объекта или всего списка
         return $vid > 0 ? $found : $ret;
     }
 
+    // Изменен метод apiSend для выполнения HTTP-запроса
     public function apiSend($url)
     {
-        return file_get_contents($url);
+        // Получение ответа от API
+        $response = file_get_contents($url);
+
+        // Проверка успешности запроса
+        if ($response === false) {
+            throw new \RuntimeException("Failed to get API response.");
+        }
+
+        return $response;
     }
 
     public function selfGetOption($optionName)
     {
-        return get_option($optionName);
+        // Метод для получения настройки (пример с использованием WordPress get_option)
+        
+        // return get_option($optionName);
     }
 }
